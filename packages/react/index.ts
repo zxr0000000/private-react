605cdc29449fc14c77429da3a698fd14f2f83e92
曾熙柔 <zengxirou@bytedance.com>
2024-07-16 23:51:40 +0800

    feat: 完成 JSX 打包

---
+++ packages/react/index.ts
@@ -0,0 +1,6 @@
+// Reacrt
+import { jsx } from './src/jsx';
+export default {
+	version: '0.0.0',
+	createElement: jsx
+};
---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,64 @@
+// ReactElement
+import { REACT_ELEMENT_TYPE } from '../../shared/ReactSymbols';
+import {
+	ElementType,
+	Key,
+	Ref,
+	Props,
+	ReactElementType
+} from '../../shared/ReactTypes';
+
+const ReactElement = function (
+	type: ElementType,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'ZengXirou' // 为了区分
+	};
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDev = jsx;
